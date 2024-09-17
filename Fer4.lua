b46b2282e009071f8c48e6c642441e1d633e85b0
Fernanflop091o <162623537+Fernanflop091o@users.noreply.github.com>
2024-09-17 13:11:02 -0500

    Create Fer4.lua

---
+++ Fer4.lua
@@ -0,0 +1,13 @@
+local player = game.Players.LocalPlayer
+local teleportService = game:GetService("TeleportService")
+local queueTeleport = (syn and syn.queue_on_teleport) or queue_on_teleport or (fluxus and fluxus.queue_on_teleport)
+
+local function onTeleport()
+    if queueTeleport then
+        queueTeleport("loadstring(game:HttpGet("https://raw.githubusercontent.com/Fernanflop091o/Queuet/refs/heads/main/Fer3.lua"))()")
+    end
+end
+
+teleportService.TeleportInitFailed:Connect(onTeleport)
+
+onTeleport()
