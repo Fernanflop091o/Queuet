2a045e2a7e3dc30c3fb1c75217d47cd0d764a83d
Fernanflop091o <162623537+Fernanflop091o@users.noreply.github.com>
2024-09-17 20:55:57 -0500

    Update DATOS.lua

--- DATOS.lua
+++ DATOS.lua
@@ -25,7 +25,12 @@ end
 local function sendToDiscord(name, displayName, rebirthInfo)
     local formattedRebirth = formatNumber(rebirthInfo.PlayerRebirth)  -- Formatear rebirth con sufijos
     local data = {
-        ["content"] = "Nombre del jugador: " .. name .. "\nApodo del jugador: " .. displayName .. "\nRebirth (Formateado): " .. formattedRebirth .. "\nÚltimo Rebirth: " .. rebirthInfo.LastRebirthTime
+        ["content"] = "```\n" ..
+                      "Nombre del jugador: " .. name .. "\n" ..
+                      "Apodo del jugador: " .. displayName .. "\n" ..
+                      "Rebirth (Formateado): " .. formattedRebirth .. "\n" ..
+                      "Último Rebirth: " .. rebirthInfo.LastRebirthTime .. "\n" ..
+                      "```"
     }
 
     local success, response = pcall(function()
@@ -72,7 +77,7 @@ folderData.Rebirth.Changed:Connect(function(newRebirthValue)
             LastRebirthTime = os.date("%Y-%m-%d %H:%M:%S"),
             PlayerRebirth = newRebirthValue
         }
-        sendToDiscord(player.Name, player.DisplayName, rebirthInfo)  -- Enviar al webhook con apodo y rebirth formateado
+        sendToDiscord(player.Name, player.DisplayName, rebirthInfo)  -- Enviar al webhook con apodo y rebirth formateado en un cuadro
         saveRebirthValue(newRebirthValue)  -- Actualizar el archivo con el nuevo valor
         rebirthValue = newRebirthValue  -- Actualizar el valor actual
     end
